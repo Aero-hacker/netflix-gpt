7480fe0221a8e49aefedc400c705d314db745c5e
Aero-hacker <jagan.devarapu@gmail.com>
2023-12-12 17:52:13 +0530

    commit

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,12 +7,18 @@ import { checkValidData } from '../utils/validate';
 
 const Login = () => {
     const [issigninform, setisigninform] = useState(true);
-    
+    const [errorMessage,seterrorMessage] = useState(null);
     const email = useRef(null);
     const password = useRef(null);
 
     const handleButtonClick = () => {
       //validate the form data
+      //check valid data (email,password)
+      console.log(email.current.value);
+      console.log(password.current.value);
+
+      const message = checkValidData(email.current.value , password.current.value);
+      seterrorMessage(message);
     }
     const togglesigninform = () => {
         setisigninform(!issigninform);
@@ -27,33 +33,40 @@ const Login = () => {
      alt='logo' 
      />
      </div>
-     <form className='w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80'>
-        <h1 className='font-bold text-3xl py-4'>
+     <form onSubmit={(e) => e.preventDefault()}
+          className='w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80'>
+          <h1 className='font-bold text-3xl py-4'>
           {issigninform? "Sign In":"Sign Up"}</h1>
-        {!issigninform && (<input 
-        type='text' 
-        placeholder='Full Name' 
-        className='p-2 my-2 w-full bg-gray-700'
-        />)}
+          {!issigninform && (<input 
+          type='text' 
+          placeholder='Full Name' 
+          className='p-2 my-2 w-full bg-gray-700'
+       />)}
         
         <input 
-        ref={email}
-        type='text' 
-        placeholder='Email Address' 
-        className='p-2 my-2 w-full bg-gray-700'/>
+          ref={email}
+          type='text' 
+          placeholder='Email Address' 
+          className='p-2 my-2 w-full bg-gray-700'
+        />
         
         <input 
-        type='text' 
-        placeholder='Password' 
-        className='p-2 my-2 w-full bg-gray-700'/>
-        <button className='p-4 my-4 bg-red-700 w-full rounded-lg'
-         onClick={handleButtonClick}> 
-        {issigninform? "Sign In":"Sign Up"}
+          ref={password}
+          type='text' 
+          placeholder='Password' 
+          className='p-2 my-2 w-full bg-gray-700'
+        />
+        <p className='text-red-500 font-bold text-lg py-2'>{errorMessage}</p>
+
+         <button 
+           className='p-4 my-4 bg-red-700 w-full rounded-lg'
+           onClick={handleButtonClick}> 
+           {issigninform? "Sign In":"Sign Up"}
          </button>
-         <p className='py-4 cursor-pointer' onClick={togglesigninform}>
-           {issigninform
-           ? "New to Netflix? Sign Up Now"
-           :"Already registered? sign in now"} 
+          <p className='py-4 cursor-pointer' onClick={togglesigninform}>
+            {issigninform
+            ? "New to Netflix? Sign Up Now"
+            :"Already registered? sign in now"} 
          </p>
      </form>
       
--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,4 +1,4 @@
-export const checkValidData = {email,password} => {
+export const checkValidData = (email, password) => {
    
  const isEmailvalid = /^[a-zA-Z0-9. _-]+@[a-zA-Z0-9. -]+\. [a-zA-Z]{2,4}$/.test(email);
  const isPasswordValid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
