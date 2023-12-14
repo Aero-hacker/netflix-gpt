6e51e9b339c209d19bb812789cee2fd0e896dc68
Aero-hacker <jagan.devarapu@gmail.com>
2023-12-14 18:37:09 +0530

    commit

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,8 @@
 import { useState,useRef } from 'react'
 import Header from './Header'
 import { checkValidData } from '../utils/validate';
- 
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from '../utils/firebase';
 
 
 
@@ -22,6 +23,28 @@ const Login = () => {
 
       const message = checkValidData( email.current.value ,password.current.value,myname.current.value);
       setErrorMessage(message);
+      if(message) return;
+
+      if(!issigninform) {
+        //signup logic
+        createUserWithEmailAndPassword(
+          auth,  
+          email.current.value ,
+          password.current.value
+          )
+        .then((userCredential) => {
+        // Signed up 
+         const user = userCredential.user;
+           console.log(user);
+           })
+           .catch((error) => {
+              const errorCode = error.code;
+              const errorMessage = error.message;
+              setErrorMessage(errorCode+"-"+errorMessage)
+  });
+      }else{
+        //signin logic
+      }
      
      }
     const togglesigninform = () => {
